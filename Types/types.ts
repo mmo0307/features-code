31d7af6fb9c947bf17d11f9489ec64cc1794400e
Mihail <65620172+mmo0307@users.noreply.github.com>
2025-05-27 22:08:21 +0300

    Update types.ts

--- Types/types.ts
+++ Types/types.ts
@@ -2,4 +2,8 @@ function keys<T extends object>(data: T): Array<keyof T> {
   return Object.keys(data) as Array<keyof T>
 }
 
+function getByKey<T, K extends keyof T>(obj: T, key: K): T[K] {
+  return obj[key];
+}
+
 type VoidFn = () => void;

cdcc1f5cb2634888aa4c3749074cc10708653335
Mihail <65620172+mmo0307@users.noreply.github.com>
2025-05-27 22:16:41 +0300

    Update types.ts

--- Types/types.ts
+++ Types/types.ts
@@ -6,4 +6,17 @@ function getByKey<T, K extends keyof T>(obj: T, key: K): T[K] {
   return obj[key];
 }
 
+//------------
+type ValueOf<T> = T[keyof T];
+
+const Color = {
+  RED: 'red',
+  GREEN: 'green'
+} as const;
+
+type Color = ValueOf<typeof Color>;
+
+const color: Color = ...
+//------------
+
 type VoidFn = () => void;
